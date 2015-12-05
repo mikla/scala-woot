ce87e425595bbd02d656d3a77dc6f996a659a41f
Artsiom Miklushou <mikla.op@gmail.com>
2015-12-05 12:46:59 +0300

    pos spec.

--- src/test/scala/model/WStringSpec.scala
+++ src/test/scala/model/WStringSpec.scala
@@ -6,6 +6,15 @@ class WStringSpec extends FlatSpec with Matchers {
 
   "pos" should "return a position of character by Id" in {
 
+    val oc = OperationClock(0)
+
+    val wstring = WString(SiteId("A"), chars = Vector(
+      WChar(CharId("A", oc.next), 'a', Beginning, Ending, isVisible = true)
+    ))
+
+    println(wstring.pos(CharId("A", OperationClock(1))))
+    println(wstring.pos(Beginning))
+
   }
 
 }
